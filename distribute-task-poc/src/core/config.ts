aa9c582a9ec00411bac1411068b2d7f05888ba9e
Bin Ren <binren@microsoft.com>
2020-02-25 15:45:16 -0800

    Redact cosmos db connection string

--- distribute-task-poc/src/core/config.ts
+++ distribute-task-poc/src/core/config.ts
@@ -2,7 +2,7 @@ const config = {
     region: 'westeurope',
     cosmos: {
         endpoint: 'https://distributedtaskpoc.documents.azure.com:443/',
-        key: '63FtSaNDKnVEffCwTs96Nfo1RK9XXeRSV0fefmBcnSDbAjAdvE93diIpH34NtHXjBENBWl8YOBZO1QUvWwx9hQ==',
+        key: '<your key>',
         database: 'POC',
         collection: 'Store'
     }
